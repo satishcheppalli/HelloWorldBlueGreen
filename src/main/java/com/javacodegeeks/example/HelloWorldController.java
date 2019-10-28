5e4704140565e21f7ec127c7face3725b432aa10
satishcheppalli <39270018+satishcheppalli@users.noreply.github.com>
2019-10-27 20:50:23 -0400

    Update HelloWorldController.java

--- src/main/java/com/javacodegeeks/example/HelloWorldController.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldController.java
@@ -8,7 +8,7 @@ public class HelloWorldController {
 
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "<font size='4' face='arial' color='#1c87c9'><b>Blue Deployment</b></font>";
-		//return "<font size='4' face='arial' color='#8ebf42'><b>Green Deployment</b></font>";
+		//return "<font size='4' face='arial' color='#1c87c9'><b>Blue Deployment</b></font>";
+		return "<font size='4' face='arial' color='#8ebf42'><b>Green Deployment</b></font>";
 	}
 }
