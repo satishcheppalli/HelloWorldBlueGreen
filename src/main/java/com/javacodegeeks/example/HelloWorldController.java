9bdd790ca99ab02de39b602efb82a23b99e80758
satishcheppalli <39270018+satishcheppalli@users.noreply.github.com>
2019-10-30 19:58:56 -0400

    Update HelloWorldController.java

--- src/main/java/com/javacodegeeks/example/HelloWorldController.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
 	@GetMapping("/hello")
-	public String sayHello() {
+	public String sayHello() throws InterruptedException {
 		Thread.sleep(30 * 1000);
 		return "<font size='4' face='arial' color='#1c87c9'><b>Blue Deployment</b></font>";
 		//return "<font size='4' face='arial' color='#8ebf42'><b>Green Deployment</b></font>";
