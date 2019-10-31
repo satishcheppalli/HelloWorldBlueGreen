f10aed4879b685a37894d045ec66e59a70e15201
satishcheppalli <39270018+satishcheppalli@users.noreply.github.com>
2019-10-30 20:02:59 -0400

    Update HelloWorldController.java

--- src/main/java/com/javacodegeeks/example/HelloWorldController.java
+++ src/main/java/com/javacodegeeks/example/HelloWorldController.java
@@ -8,6 +8,9 @@ public class HelloWorldController {
 
 	@GetMapping("/hello")
 	public String sayHello() throws InterruptedException {
+		for(int i=0; i<=5000;i++){
+			System.out.println(i);
+		}
 		Thread.sleep(30 * 1000);
 		return "<font size='4' face='arial' color='#1c87c9'><b>Blue Deployment</b></font>";
 		//return "<font size='4' face='arial' color='#8ebf42'><b>Green Deployment</b></font>";
